96f89b02cf044c07a440b1f855c9c22ae833199d
PramodhaKasam <kasam.pramodha@gmail.com>
2024-06-01 13:12:37 +0530

    update again

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home.js'
 import Contact from './components/Contacts.js'
 import Trans from './components/Trans.js'
 import Login from './components/Login.js'
+import Data from './components/data.js'
 
 function App() {
   
@@ -19,6 +20,7 @@ function App() {
             <Route path="/Home" element={<Home/>}/>
             <Route path="/Contact" element={<Contact/>}/>
             <Route path="/Trans/:name" element={<Trans/>}/>
+            <Route path="/Data" element={<Data/>}/>
             
 
             
---
+++ src/components/data.js
@@ -0,0 +1,11 @@
+import './data.css'
+import { Link } from 'react-router-dom'
+
+function Data()
+{
+    return (
+
+    )
+}
+
+export default Data;
